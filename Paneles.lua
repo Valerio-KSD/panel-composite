5c2a72ca4760df1a6a258fa98a9d0c865209f78a
joaquinsanmar@gmail.com <joaquinsanmar@gmail.com>
2021-02-25 15:13:31 +0100

    Correccion de algunos errores en "dibujarCorteAH" y desarrollo de la funcion "dibujarFresadoBH"

--- Paneles.lua
+++ Paneles.lua
@@ -3,27 +3,44 @@
 
 require "strict"
 
+--Entradas comunes a todos los modelos [AH][BH][CH]
 alaSuperior=40
 alaInferior=40
-alaIzquierda=40
-alaDerecha=40--64
-
-margenA=57 --Margen Superior
-margenB=108--132 --Margen Inferior
-
-anchuraPlaca  = 1000
-margenPlaca  = 3
-alturaPlaca  = 500
+alaDerecha=40
+alaIzquierda=alaDerecha
 
 pliegueSuperior=36
 pliegueInferior=36
+
+margenA=57 --Margen Superior
+margenB=108 --132 --Margen Inferior
+
+anchuraPlaca = 2000 --1000
+margenPlaca = 3
+alturaPlaca = 1000 --500
+
+
 
 origenX=0
 origenY=0
 
 
-modelo=2 --[AH=0 - AH1=1 - AH2=2 - AH3=3 - AH4=4 - AH5=5 - AH6=6 - AH7=7 - AH8=8]
+--Entrada para modelo [BH]
+anchura1=1200 --izquierda
+anchura2=800 --derecha
+anchuraPlaca=anchura1+anchura2
 
+
+
+
+modelo=17
+--[AH=0 - AH1=1 - AH2=2 - AH3=3 - AH4=4 - AH5=5 - AH6=6 - AH7=7 - AH8=8]
+--[BH=9 - BH1=10 - BH2=11 - BH3=12 - BH4=13 - BH5=14 - BH6=15 - BH7=16 - BH8=17]
+--[BH9=18 - BH10=19 - BH11=20 - BH12=21 - BH13=22 - BH14=23 - BH15=24 - BH16=25 - BH17=26]
+--[CH=27 - CH1=28 - CH2=329 - CH3=30 - CH4=31 - CH5=32 - CH6=33 - CH7=34 - CH8=35]
+
+
+ 
 
 ruta=""
 doc = VectricJob()
@@ -54,7 +71,6 @@ function main(script_path)
 		
 	return true; 
 end 
-
 
 
 function OnLuaButton_modeloAH(framePrincipal)
@@ -112,9 +128,16 @@ function OnLuaButton_aceptarAH(modeloAH)
 	
     	
 	else
-	
-		dibujarFresadoAH(doc,anchuraPlaca,alturaPlaca,margenPlaca, origenX,origenY,modelo)
-		dibujarCorteAH(doc,anchuraPlaca,alturaPlaca,margenPlaca,origenX,origenY,modelo)
+		
+		
+		
+		
+		dibujarFresadoAH(doc)
+		dibujarCorteAH(doc)
+		
+		
+		
+		--dibujarFresadoBH(doc)
 		
 		
 		DisplayMessageBox("Modelo AH Creado Correctamente")
@@ -127,8 +150,7 @@ end
 
 
 
-
-function dibujarFresadoAH(doc,anchuraPlaca,alturaPlaca,margenPlaca,origenX,origenY,modelo)
+function dibujarFresadoAH(doc)
 	
 	local Contour = Contour(0.0)
 	
@@ -145,7 +167,7 @@ function dibujarFresadoAH(doc,anchuraPlaca,alturaPlaca,margenPlaca,origenX,orige
 	local fresado11 = Point2D(origenX+anchuraPlaca+alaIzquierda,origenY+alaInferior+pliegueInferior+alturaPlaca+pliegueSuperior+alaSuperior)
 	
 	
-	if modelo==0 then --REF AH
+	if modelo==0 or modelo==1 then --REF [AH][AH1]
 		Contour:AppendPoint(fresado9)
 		Contour:LineTo(fresado2)
 		Contour:LineTo(fresado3)
@@ -156,18 +178,7 @@ function dibujarFresadoAH(doc,anchuraPlaca,alturaPlaca,margenPlaca,origenX,orige
 		Contour:LineTo(fresado8)
 		Contour:LineTo(fresado7)
 		Contour:LineTo(fresado2)
-	elseif modelo==1 then --REF AH1
-		Contour:AppendPoint(fresado9)
-		Contour:LineTo(fresado2)
-		Contour:LineTo(fresado3)
-		Contour:LineTo(fresado10)
-		Contour:LineTo(fresado5)
-		Contour:LineTo(fresado6)
-		Contour:LineTo(fresado7)
-		Contour:LineTo(fresado8)
-		Contour:LineTo(fresado7)
-		Contour:LineTo(fresado1)
-	elseif modelo==2 then --REF AH2
+	elseif modelo==2 or modelo==3 then --REF [AH2][AH3]
 		Contour:AppendPoint(fresado1)
 		Contour:LineTo(fresado2)
 		Contour:LineTo(fresado3)
@@ -178,18 +189,7 @@ function dibujarFresadoAH(doc,anchuraPlaca,alturaPlaca,margenPlaca,origenX,orige
 		Contour:LineTo(fresado4)
 		Contour:LineTo(fresado8)
 		Contour:LineTo(fresado7)
-	elseif modelo==3 then --REF AH3
-		Contour:AppendPoint(fresado1)
-		Contour:LineTo(fresado2)
-		Contour:LineTo(fresado3)
-		Contour:LineTo(fresado4)
-		Contour:LineTo(fresado1)
-		Contour:LineTo(fresado6)
-		Contour:LineTo(fresado5)
-		Contour:LineTo(fresado4)
-		Contour:LineTo(fresado8)
-		Contour:LineTo(fresado7)
-	elseif modelo==4 then --REF AH4
+	elseif modelo==4 then --REF [AH4]
 		Contour:AppendPoint(fresado9)
 		Contour:LineTo(fresado2)
 		Contour:LineTo(fresado3)
@@ -199,7 +199,7 @@ function dibujarFresadoAH(doc,anchuraPlaca,alturaPlaca,margenPlaca,origenX,orige
 		Contour:LineTo(fresado7)
 		Contour:LineTo(fresado6)
 		Contour:LineTo(fresado1)
-	elseif modelo==5 then --REF AH5
+	elseif modelo==5 then --REF [AH5]
 		Contour:AppendPoint(fresado1)
 		Contour:LineTo(fresado2)
 		Contour:LineTo(fresado3)
@@ -210,7 +210,7 @@ function dibujarFresadoAH(doc,anchuraPlaca,alturaPlaca,margenPlaca,origenX,orige
 		Contour:LineTo(fresado6)
 		Contour:LineTo(fresado1)
 		Contour:LineTo(fresado4)
-	elseif modelo==6 then --REF AH6
+	elseif modelo==6 then --REF [AH6]
 		Contour:AppendPoint(fresado9)
 		Contour:LineTo(fresado2)
 		Contour:LineTo(fresado3)
@@ -222,7 +222,7 @@ function dibujarFresadoAH(doc,anchuraPlaca,alturaPlaca,margenPlaca,origenX,orige
 		Contour:LineTo(fresado8)
 		Contour:LineTo(fresado7)
 		Contour:LineTo(fresado1)
-	elseif modelo==7 then --REF AH7
+	elseif modelo==7 then --REF [AH7]
 		Contour:AppendPoint(fresado1)
 		Contour:LineTo(fresado2)
 		Contour:LineTo(fresado3)
@@ -235,7 +235,7 @@ function dibujarFresadoAH(doc,anchuraPlaca,alturaPlaca,margenPlaca,origenX,orige
 		Contour:LineTo(fresado7)
 		Contour:LineTo(fresado8)
 		Contour:LineTo(fresado4)
-	else  --REF AH8
+	elseif modelo==8 then  --REF [AH8]
 		Contour:AppendPoint(fresado9)
 		Contour:LineTo(fresado2)
 		Contour:LineTo(fresado3)
@@ -256,7 +256,7 @@ function dibujarFresadoAH(doc,anchuraPlaca,alturaPlaca,margenPlaca,origenX,orige
 	local cur_layer = doc.LayerManager:GetActiveLayer()
 	local layer = doc.LayerManager:GetLayerWithName("Fresado")
 	layer:AddObject(cad_object, true)
-	layer:SetColor(0,0,0) --(0,255,0)
+	layer:SetColor(0.3,0.8,0.4)
 	layer.Visible = true 
 	doc.LayerManager:SetActiveLayer(cur_layer)
 	doc:Refresh2DView()	
@@ -268,7 +268,7 @@ end
 
 
 
-function dibujarCorteAH(doc,anchuraPlaca,alturaPlaca,margenPlaca,origenX,origenY,modelo)
+function dibujarCorteAH(doc)
 	
 	local Contour = Contour(0.0)
 	
@@ -348,19 +348,17 @@ function dibujarCorteAH(doc,anchuraPlaca,alturaPlaca,margenPlaca,origenX,origenY
 	
 	
 		--Variables MARGENES superior e inferior
-
-	
-	local cotaInicio=(margenB-alturaBotaDeCuelgue)*(36.8/52) --36.8 
+	local cotaInicioDerecha=(margenB-alturaBotaDeCuelgue)*(36.8/52) --36.8 
 	
 	local anchoLateral1=alaDerecha-anchoBota  --21.5 --variable para reducir codigo (se podria eliminar)
-	local alturaLateral1=cotaInicio-alaDerecha+anchoBota --15.3  --variable para reducir codigo (se podria eliminar)
+	local alturaLateral1=cotaInicioDerecha-alaDerecha+anchoBota --15.3  --variable para reducir codigo (se podria eliminar)
 	
 	local sumaComienzoFin=(margenA)+(margenB-alturaBotaDeCuelgue) --109
 	
 	
 	--Parte inicial
-	local corte20 = Point2D(origenX+alaIzquierda+anchuraPlaca+cotaInicio,origenY+alaInferior+pliegueInferior+cotaInicio)
-	local corte21 = Point2D(origenX+alaIzquierda+anchuraPlaca+anchoLateral1,origenY+alaInferior+pliegueInferior+alturaLateral1+cotaInicio)
+	local corte20 = Point2D(origenX+alaIzquierda+anchuraPlaca+cotaInicioDerecha,origenY+alaInferior+pliegueInferior+cotaInicioDerecha)
+	local corte21 = Point2D(origenX+alaIzquierda+anchuraPlaca+anchoLateral1,origenY+alaInferior+pliegueInferior+alturaLateral1+cotaInicioDerecha)
 	Contour:LineTo(corte20)
 	Contour:LineTo(corte21)
 	
@@ -369,7 +367,7 @@ function dibujarCorteAH(doc,anchuraPlaca,alturaPlaca,margenPlaca,origenX,origenY
 	local tramoRectoEntreBotaDeCuelgue=(alturaPlaca-sumaComienzoFin-((repeticiones+1)*alturaBotaDeCuelgue)-(repeticiones*anchoBota))/repeticiones
 	
 	local puntoInicioX=(origenX+alaIzquierda+anchuraPlaca+anchoLateral1)
-	local puntoInicioY=(origenY+alaInferior+pliegueInferior+cotaInicio+alturaLateral1)
+	local puntoInicioY=(origenY+alaInferior+pliegueInferior+cotaInicioDerecha+alturaLateral1)
 	
 	for n=1, (repeticiones+1) do
 		--Dibujar bota
@@ -402,7 +400,7 @@ function dibujarCorteAH(doc,anchuraPlaca,alturaPlaca,margenPlaca,origenX,origenY
 		puntoInicioY=puntoInicioY+tramoRecto4BotaDerecha
 		
 		--Dibujar tramo recto
-		if n==repeticiones+1 then
+		if n==(repeticiones+1) then
 			break
 		else
 			local corte14=Point2D(puntoInicioX,puntoInicioY+tramoRectoEntreBotaDeCuelgue)
@@ -487,14 +485,24 @@ function dibujarCorteAH(doc,anchuraPlaca,alturaPlaca,margenPlaca,origenX,origenY
 	local tramoRecto3BotaIzquierda=18
 	local tramoRecto4BotaIzquierda=52
 	
+		--Variables por si el ala derecha e izquierda no son simetricas
+	local margenA_izq = margenA --Al igualarlo al tamaño de los margenes de la derecha la figura es simetrica, si deja de ser simetrica, hay que pedirle que introduzca manualmente los margenes de la izquierda
+	local margenB_izq = margenB
+	local cotaInicioIzquierda=(margenB_izq-alturaBotaDeCuelgue)*(36.8/52)
+	
 	
 	--Parte inicial
+	local lineaIzquierda=(margenA_izq)*(17/57)
+	
 	local corte60 = Point2D(origenX,origenY+alaInferior+pliegueInferior+alturaPlaca-alaIzquierda)
 	Contour:LineTo(corte60)
+	local corte61 = Point2D(origenX,origenY+alaInferior+pliegueInferior+alturaPlaca-alaIzquierda-lineaIzquierda)
+	Contour:LineTo(corte61)
+	
 	
 	--Parte botas
 	local puntoInicioX=(origenX)
-	local puntoInicioY=(origenY+alturaPlaca+alaInferior+pliegueInferior-margenA)
+	local puntoInicioY=(origenY+alaInferior+pliegueInferior+alturaPlaca-margenA_izq)
 		
 	for n=1, (repeticiones+1) do
 		--Dibujar bota
@@ -527,16 +535,14 @@ function dibujarCorteAH(doc,anchuraPlaca,alturaPlaca,margenPlaca,origenX,origenY
 		Contour:LineTo(corte39)
 		puntoInicioY=puntoInicioY-tramoRecto4BotaIzquierda
 		
-		
-		
 		--Dibujar tramo recto
-		if n==repeticiones+1 then
+		if n==(repeticiones+1) then
 			break
 		else
-			local corte40=Point2D(puntoInicioX-(alaIzquierda-anchoLateral1),puntoInicioY-(alaIzquierda-anchoLateral1))
+			local corte40=Point2D(puntoInicioX-(anchoBota),puntoInicioY-(anchoBota))
 			Contour:LineTo(corte40)
-			puntoInicioY=puntoInicioY-(alaIzquierda-anchoLateral1)
-			puntoInicioX=puntoInicioX-(alaIzquierda-anchoLateral1)
+			puntoInicioY=puntoInicioY-(anchoBota)
+			puntoInicioX=puntoInicioX-(anchoBota)
 			
 			
 			local corte41=Point2D(puntoInicioX,puntoInicioY-tramoRectoEntreBotaDeCuelgue)
@@ -547,8 +553,8 @@ function dibujarCorteAH(doc,anchuraPlaca,alturaPlaca,margenPlaca,origenX,origenY
 
 	end
 	
-	--final
-	local corte65 = Point2D(origenX+alaIzquierda-cotaInicio,origenY+alaInferior+pliegueInferior+cotaInicio)
+	--Parte final
+	local corte65 = Point2D(origenX+alaIzquierda-cotaInicioIzquierda,origenY+alaInferior+pliegueInferior+cotaInicioIzquierda)
 	local corte66 = Point2D(origenX+alaIzquierda,origenY+alaInferior+pliegueInferior)
 	Contour:LineTo(corte65)
 	Contour:LineTo(corte66)
@@ -559,7 +565,7 @@ function dibujarCorteAH(doc,anchuraPlaca,alturaPlaca,margenPlaca,origenX,origenY
 	local cur_layer = doc.LayerManager:GetActiveLayer()
 	local layer = doc.LayerManager:GetLayerWithName("Corte")
 	layer:AddObject(cad_object, true)
-	layer:SetColor(255,0,0)
+    layer:SetColor(0.9,0.2,0.2)
 	layer.Visible = true 
 	doc.LayerManager:SetActiveLayer(cur_layer)
 	doc:Refresh2DView()	
@@ -570,7 +576,7 @@ end
 
 
 
-function Dibujar_Taladros(doc,anchuraPlaca,alturaPlaca,margenPlaca,alturaAla)
+function dibujarTaldrosAH(doc)
 
 	local Contour = Contour(0.0)
 
@@ -583,10 +589,208 @@ function Dibujar_Taladros(doc,anchuraPlaca,alturaPlaca,margenPlaca,alturaAla)
 	local cur_layer = doc.LayerManager:GetActiveLayer()
 	local layer = doc.LayerManager:GetLayerWithName("Taladros")
 	layer:AddObject(cad_object, true)
-	layer.Colour = 255255255 
+	layer:SetColor(0.1,0.1,0.1)
 	layer.Visible = true 
 	doc.LayerManager:SetActiveLayer(cur_layer)
 	doc:Refresh2DView()	
 	-----------------------------------------------------------------
 	return true; 
 end
+
+
+
+function dibujarFresadoBH(doc)
+	
+	--Variables
+	local separacionPliegues=4
+	
+	--Puntos trayectoria
+	local Contour = Contour(0.0)
+	
+	
+	local fresado1 = Point2D(origenX+alaIzquierda,origenY)
+	local fresado2 = Point2D(origenX+alaIzquierda,origenY+alaInferior)
+	local fresado3 = Point2D(origenX+alaIzquierda,origenY+alaInferior+pliegueInferior)
+	local fresado4 = Point2D(origenX+alaIzquierda+anchuraPlaca,origenY+alaInferior+pliegueInferior)
+	local fresado5 = Point2D(origenX+alaIzquierda+anchuraPlaca,origenY+alaInferior)
+	local fresado6 = Point2D(origenX+alaIzquierda+anchuraPlaca,origenY)
+	local fresado7 = Point2D(origenX+alaIzquierda+anchura1,origenY+alaInferior+pliegueInferior)
+	
+	local fresado8
+	local fresado9
+	
+	local fresado10 = Point2D(origenX+alaIzquierda,origenY+alaInferior+pliegueInferior+alturaPlaca)
+	local fresado11 = Point2D(origenX+alaIzquierda,origenY+alaInferior+pliegueInferior+alturaPlaca+pliegueSuperior)
+	
+	local fresado12
+	local fresado13
+	
+	local fresado14 = Point2D(origenX+alaIzquierda+anchura1+anchura2,origenY+alaInferior+pliegueInferior+alturaPlaca+pliegueSuperior)
+	local fresado15 = Point2D(origenX+alaIzquierda+anchura1+anchura2,origenY+alaInferior+pliegueInferior+alturaPlaca)
+	local fresado16 = Point2D(origenX+alaIzquierda+anchura1,origenY+alaInferior+pliegueInferior+alturaPlaca)
+	local fresado17 = Point2D(origenX+alaIzquierda+anchura1+anchura2,origenY+alaInferior+pliegueInferior+alturaPlaca+pliegueSuperior+alaSuperior)
+	
+	
+	modelo=24
+	
+	if modelo>=10 and modelo<=18 then
+		fresado8 = Point2D(origenX+alaIzquierda+anchura1-pliegueInferior,origenY+alaInferior)
+		fresado9 = Point2D(origenX+alaIzquierda+anchura1+pliegueInferior,origenY+alaInferior)
+		fresado12 = Point2D(origenX+alaIzquierda+anchura1-pliegueSuperior,origenY+alaInferior+pliegueInferior+alturaPlaca+pliegueSuperior)
+		fresado13 = Point2D(origenX+alaIzquierda+anchura1+pliegueSuperior,origenY+alaInferior+pliegueInferior+alturaPlaca+pliegueSuperior)
+	elseif modelo>=19 and modelo<=27 then
+		fresado8 = Point2D(origenX+alaIzquierda+anchura1-(separacionPliegues/2),origenY+alaInferior)	
+		fresado9 = Point2D(origenX+alaIzquierda+anchura1+(separacionPliegues/2),origenY+alaInferior)
+		fresado12 = Point2D(origenX+alaIzquierda+anchura1-(separacionPliegues/2),origenY+alaInferior+pliegueInferior+alturaPlaca+pliegueSuperior)
+		fresado13 = Point2D(origenX+alaIzquierda+anchura1+(separacionPliegues/2),origenY+alaInferior+pliegueInferior+alturaPlaca+pliegueSuperior)
+	end
+	
+	
+	if modelo==9 or modelo==10 or modelo==18 or modelo==19  then --REF [BH][BH1] [BH9][BH10]
+		Contour:AppendPoint(fresado1)
+		Contour:LineTo(fresado3)
+		Contour:LineTo(fresado4)
+		Contour:LineTo(fresado6)
+		Contour:LineTo(fresado14)
+		Contour:LineTo(fresado13)
+		Contour:LineTo(fresado14)
+		Contour:LineTo(fresado15)
+		Contour:LineTo(fresado10)
+		Contour:LineTo(fresado11)
+		Contour:LineTo(fresado12)
+		Contour:LineTo(fresado11)
+		Contour:LineTo(fresado10)
+		Contour:LineTo(fresado16)
+		Contour:LineTo(fresado7)
+		Contour:LineTo(fresado3)
+		Contour:LineTo(fresado10)
+		
+	elseif modelo==11 or modelo==12  or modelo==20 or modelo==21  then --REF [BH2][BH3] [BH11][BH12]
+		Contour:AppendPoint(fresado8)
+		Contour:LineTo(fresado2)
+		Contour:LineTo(fresado3)
+		Contour:LineTo(fresado4)
+		Contour:LineTo(fresado5)
+		Contour:LineTo(fresado9)
+		Contour:LineTo(fresado5)
+		Contour:LineTo(fresado14)
+		Contour:LineTo(fresado13)
+		Contour:LineTo(fresado14)
+		Contour:LineTo(fresado15)
+		Contour:LineTo(fresado16)
+		Contour:LineTo(fresado7)
+		Contour:LineTo(fresado3)
+		Contour:LineTo(fresado11)
+		Contour:LineTo(fresado12)
+		Contour:LineTo(fresado11)
+		Contour:LineTo(fresado10)
+		Contour:LineTo(fresado15)
+		
+	elseif modelo==14 or modelo==22  then --REF [BH4]  [BH13]
+		Contour:AppendPoint(fresado1)
+		Contour:LineTo(fresado3)
+		Contour:LineTo(fresado4)
+		Contour:LineTo(fresado6)
+		Contour:LineTo(fresado14)
+		Contour:LineTo(fresado15)
+		Contour:LineTo(fresado10)
+		Contour:LineTo(fresado11)
+		Contour:LineTo(fresado3)
+		Contour:LineTo(fresado7)
+		Contour:LineTo(fresado16)
+		
+	elseif modelo==14 or modelo==23  then --REF [BH5]  [BH14]
+		Contour:AppendPoint(fresado8)
+		Contour:LineTo(fresado2)
+		Contour:LineTo(fresado3)
+		Contour:LineTo(fresado4)
+		Contour:LineTo(fresado5)
+		Contour:LineTo(fresado9)
+		Contour:LineTo(fresado5)
+		Contour:LineTo(fresado14)
+		Contour:LineTo(fresado15)
+		Contour:LineTo(fresado10)
+		Contour:LineTo(fresado11)
+		Contour:LineTo(fresado3)
+		Contour:LineTo(fresado7)
+		Contour:LineTo(fresado16)
+		
+	elseif modelo==15 or modelo==24  then --REF [BH6] [BH15]
+		Contour:AppendPoint(fresado1)
+		Contour:LineTo(fresado3)
+		Contour:LineTo(fresado4)
+		Contour:LineTo(fresado6)
+		Contour:LineTo(fresado17)
+		Contour:LineTo(fresado14)
+		Contour:LineTo(fresado13)
+		Contour:LineTo(fresado14)
+		Contour:LineTo(fresado15)
+		Contour:LineTo(fresado10)
+		Contour:LineTo(fresado11)
+		Contour:LineTo(fresado12)
+		Contour:LineTo(fresado11)
+		Contour:LineTo(fresado3)
+		Contour:LineTo(fresado7)
+		Contour:LineTo(fresado16)
+		
+	elseif modelo==16 or modelo==25  then --REF [BH7]  [BH16]
+		Contour:AppendPoint(fresado8)
+		Contour:LineTo(fresado2)
+		Contour:LineTo(fresado3)
+		Contour:LineTo(fresado4)
+		Contour:LineTo(fresado5)
+		Contour:LineTo(fresado9)
+		Contour:LineTo(fresado5)
+		Contour:LineTo(fresado17)
+		Contour:LineTo(fresado14)
+		Contour:LineTo(fresado13)
+		Contour:LineTo(fresado14)
+		Contour:LineTo(fresado15)
+		Contour:LineTo(fresado10)
+		Contour:LineTo(fresado11)
+		Contour:LineTo(fresado12)
+		Contour:LineTo(fresado11)
+		Contour:LineTo(fresado3)
+		Contour:LineTo(fresado7)
+		Contour:LineTo(fresado16)
+		
+	elseif modelo==17 or modelo==26  then --REF [BH8] [BH17]
+		Contour:AppendPoint(fresado8)
+		Contour:LineTo(fresado2)
+		Contour:LineTo(fresado1)
+		Contour:LineTo(fresado3)
+		Contour:LineTo(fresado4)
+		Contour:LineTo(fresado6)
+		Contour:LineTo(fresado5)
+		Contour:LineTo(fresado9)
+		Contour:LineTo(fresado5)
+		Contour:LineTo(fresado14)
+		Contour:LineTo(fresado13)
+		Contour:LineTo(fresado14)
+		Contour:LineTo(fresado15)
+		Contour:LineTo(fresado10)
+		Contour:LineTo(fresado11)
+		Contour:LineTo(fresado12)
+		Contour:LineTo(fresado11)
+		Contour:LineTo(fresado3)
+		Contour:LineTo(fresado7)
+		Contour:LineTo(fresado16)	
+	end
+	
+	
+	--------------------------------------------------------
+	local cad_object = CreateCadContour(Contour)
+	local cur_layer = doc.LayerManager:GetActiveLayer()
+	local layer = doc.LayerManager:GetLayerWithName("Fresado")
+	layer:AddObject(cad_object, true)
+	layer:(0.3,0.8,0.4)
+	layer.Visible = true 
+	doc.LayerManager:SetActiveLayer(cur_layer)
+	doc:Refresh2DView()	
+	-----------------------------------------------------------------
+	return true; 
+end
+
+
+
+
